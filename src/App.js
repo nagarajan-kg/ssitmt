ec511e77ccc2b55a5e2aef1a5ff5f7983aeeee7e
Nagarajan KG <88096994+nagarajan-kg@users.noreply.github.com>
2025-11-20 10:10:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
-import React from "react";
+// import React from "react";
+// import Home from "./pages/Home";
+
+// export default function App() {
+//   return <Home />;
+// }
+
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 import Home from "./pages/Home";
 
 export default function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,      // animation speed
+      once: true,          // one-time animation
+      disable: "mobile",   // MOBILE devices-ல AOS disable ஆகும்
+    });
+  }, []);
+
   return <Home />;
 }
